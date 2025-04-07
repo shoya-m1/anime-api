7cfb60044acf36c3df380d8234a6a23eee3e81cd
shoya-m1 <ilmimusthofa8189@gamil.com>
2025-04-08 00:52:52 +0700

    update package.json

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const app = require('./src/app');
+const express = require("express");
+const serverless = require("serverless-http"); // tambahkan ini
+const app = express();
 
-const PORT = process.env.PORT || 3000;
+const animeRoutes = require("./src/routes/animeRoutes");
+const episodeRoutes = require("./src/routes/episodeRoutes");
+const serverRoutes = require("./src/routes/server");
+const defaultRoute = require("./src/routes/defaultRoute");
+const notFound = require("./src/routes/notFound");
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/", defaultRoute);
+app.use("/anime", animeRoutes);
+app.use("/episode", episodeRoutes);
+app.use("/server", serverRoutes);
+app.use(notFound);
+
+module.exports = serverless(app);
--- src/app.js
+++ src/app.js
@@ -1,22 +1,22 @@
-const express = require("express");
-const serverless = require("serverless-http"); // tambahkan ini
-const app = express();
+// const express = require("express");
+// const serverless = require("serverless-http"); // tambahkan ini
+// const app = express();
 
-const animeRoutes = require("./routes/animeRoutes");
-const episodeRoutes = require("./routes/episodeRoutes");
-const serverRoutes = require("./routes/server");
-const defaultRoute = require("./routes/defaultRoute");
-const notFound = require("./routes/notFound");
+// const animeRoutes = require("./routes/animeRoutes");
+// const episodeRoutes = require("./routes/episodeRoutes");
+// const serverRoutes = require("./routes/server");
+// const defaultRoute = require("./routes/defaultRoute");
+// const notFound = require("./routes/notFound");
 
-const cors = require("cors");
+// const cors = require("cors");
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
-app.use("/", defaultRoute);
-app.use("/anime", animeRoutes);
-app.use("/episode", episodeRoutes);
-app.use("/server", serverRoutes);
-app.use(notFound);
+// app.use("/", defaultRoute);
+// app.use("/anime", animeRoutes);
+// app.use("/episode", episodeRoutes);
+// app.use("/server", serverRoutes);
+// app.use(notFound);
 
-module.exports = serverless(app);
+// module.exports = serverless(app);
