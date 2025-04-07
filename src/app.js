da26ae020af0705ee49c33104d8b30df62a5d13e
shoya-m1 <ilmimusthofa8189@gamil.com>
2025-04-08 00:15:53 +0700

    update app.js

--- src/app.js
+++ src/app.js
@@ -1,25 +1,22 @@
 const express = require("express");
-const animeRoutes = require("./routes/animeRoutes");
-const episodeRoutes = require("./routes/episodeRoutes");
-const serverRoute = require("./routes/server");
-const notFound = require("./routes/notFound");
-const defaultRoute = require("./routes/defaultRoute");
-const cors = require("cors");
+const serverless = require("serverless-http"); // tambahkan ini
 const app = express();
 
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type"],
-  })
-);
+const animeRoutes = require("./routes/animeRoutes");
+const episodeRoutes = require("./routes/episodeRoutes");
+const serverRoutes = require("./routes/server");
+const defaultRoute = require("./routes/defaultRoute");
+const notFound = require("./routes/notFound");
 
+const cors = require("cors");
+
+app.use(cors());
 app.use(express.json());
+
 app.use("/", defaultRoute);
 app.use("/anime", animeRoutes);
 app.use("/episode", episodeRoutes);
-app.use("/server", serverRoute);
+app.use("/server", serverRoutes);
 app.use(notFound);
 
-module.exports = app;
+module.exports = serverless(app);
