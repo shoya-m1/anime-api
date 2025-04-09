6ee004d41d6e8f079cc5427a2f2c1559582c07b2
shoya-m1 <ilmimusthofa8189@gamil.com>
2025-04-09 21:19:13 +0700

    update route

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,7 +11,10 @@ router.get("/test", async (req, res) => {
   try {
     const response = await axios.get("https://otakudesu.cloud/episode/snf-episode-27-sub-indo/", {
       headers: {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+        Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9",
         Referer: "https://otakudesu.cloud/",
       },
     });
